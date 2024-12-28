c308bb94f30993e81d8864a0abcc95a33d1ba5d7
Angel Yamir <innovogen375@gmail.com>
2024-12-27 20:08:13 -0600

    se han agregado vistas anidadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,23 @@
 import { Routes } from '@angular/router';
 
 import { ListComponent } from "./domains/products/pages/list/list.component";
+import { LayoutComponent } from "@shared/components/layout/layout.component";
 import { AboutComponent } from "./domains/info/pages/about/about.component";
 import { NotFoundComponent } from "./domains/info/pages/not-found/not-found.component";
 export const routes: Routes = [
-    {
-        path: '',
-        component: ListComponent
-    },
-    {
-        path: 'about',
-        component: AboutComponent
+    {//esto es para generar vistas anidadass (componentes compartidos), se va a mostrar el layoutcomponent en cada elemento que tenga como hijo, y no tener que ponerlo uno por uno
+        path:'',
+        component: LayoutComponent,
+        children: [
+            {
+                path: '',
+                component: ListComponent
+            },
+            {
+                path: 'about',
+                component: AboutComponent
+            }
+        ]
     },
     {
         path: '**',//este debe ir al final y tiene que nener ** para que sirva como notFound
