a1c23775e75722b89e0f98c360c126216b3a97a6
Angel Yamir <innovogen375@gmail.com>
2024-12-27 19:53:31 -0600

    se a agregadoo la paguina de notFound y se a implementado el uso de routRoulink

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { CounterComponent } from "@shared/components/counter/counter.component";
+import { HeaderComponent } from "@shared/components/header/header.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, CounterComponent],
+  imports: [CommonModule, CounterComponent, HeaderComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
